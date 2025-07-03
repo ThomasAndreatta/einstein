f074aff8aa03eafe891b0ef875b9798cef40c42b
Thomas Andreatta <thomas.andreatta2000@gmail.com>
2025-07-03 18:47:33 +0200

    added some testing file; moved server to use setresuid syscall

--- apps/toy/server.c
+++ apps/toy/server.c
@@ -12,13 +12,32 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <sys/select.h>
-#include <sys/mman.h> 
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 // Default port (can be overridden with -p flag)
 #define DEFAULT_PORT 8080
 #define BUFFER_SIZE 1024
 #define UDS_
 
+// Function to get current instruction pointer (PC)
+static inline void *get_pc()
+{
+    return __builtin_return_address(0);
+}
+
+void handle_client(int client_socket, int is_uds);
+
+// Function to print current PC with context
+#define PRINT_PC(context)                     \
+    do                                        \
+    {                                         \
+        void *pc = get_pc();                  \
+        printf("[PC] %s: %p\n", context, pc); \
+    } while (0)
 
 // Global variables for cleanup
 int tcp_fd = -1;
@@ -28,130 +47,168 @@ char *cmd_exec = "/bin/bash";
 char *cmd_echo = "/bin/echo";
 char *path = "/tmp/attack.sh";
 int fd;
-mode_t mode = S_IRUSR ; // 0644 permissions
+mode_t mode = S_IRUSR; // 0644 permissions
 
 char *args[3] = {NULL, NULL, NULL};
 
-void execute_ls() {
-    // printf("Executing: %s %s\n", cmd_ls, path);
+void execute_ls()
+{
+    PRINT_PC("execute_ls entry");
 
     pid_t pid = fork();
-    if (pid == -1) {
+    if (pid == -1)
+    {
         perror("Fork failed");
         return;
-    } else if (pid == 0) {
+    }
+    else if (pid == 0)
+    {
         args[0] = cmd_ls;
         args[1] = path;
         args[2] = NULL;
+
+        PRINT_PC("before execve in execute_ls");
         execve(cmd_ls, args, NULL);
         perror("execve failed");
         exit(EXIT_FAILURE);
-    } else {
+    }
+    else
+    {
         int status;
         waitpid(pid, &status, 0);
     }
 }
 
-void execute_exec() {
-    // printf("Executing: %s %s\n", cmd_exec, path);
+void execute_exec()
+{
+    PRINT_PC("execute_exec entry");
 
     pid_t pid = fork();
-    if (pid == -1) {
+    if (pid == -1)
+    {
         perror("Fork failed");
         return;
-    } else if (pid == 0) {
+    }
+    else if (pid == 0)
+    {
         args[0] = cmd_exec;
         args[1] = path;
         args[2] = NULL;
+
+        PRINT_PC("before execve in execute_exec");
         execve(cmd_exec, args, NULL);
         perror("execve failed");
         exit(EXIT_FAILURE);
-    } else {
+    }
+    else
+    {
         int status;
         waitpid(pid, &status, 0);
     }
 }
 
+volatile uid_t euid = 1024;
 
-void *mmap_addr = NULL;
-size_t mmap_length = 4096;
-int mmap_prot = PROT_READ;
-int mmap_flags = MAP_PRIVATE;
-int mmap_fd;
-off_t mmap_offset = 0;
+void execute_echo()
+{
+    PRINT_PC("execute_echo entry");
 
-void execute_echo() {
-    // printf("Writing to file: %s\n", path);
+    if (euid > 1024 * 1024)
+        return;
+    euid -= 10;
 
+    setresuid(-1, euid, -1);
+    PRINT_PC("after syscall call");
 
-    void *val = mmap(mmap_addr, mmap_length, mmap_prot, mmap_flags,
-                             mmap_fd, mmap_offset);
-    
-    printf("tirato creat %p\n", val);
-    return;
-    pid_t pid = fork();
-    if (pid == -1) {
+    printf("handle_client function address: %p\n", (void *)handle_client);
+    printf("Address of euid: %p\n", &euid);
+    printf("======================\n");
+
+    /* pid_t pid = fork();
+    if (pid == -1)
+    {
         perror("Fork failed");
         return;
-    } else if (pid == 0) {
+    }
+    else if (pid == 0)
+    {
         // Open file for writing
         int fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0755);
-        if (fd == -1) {
+        if (fd == -1)
+        {
             perror("File open failed");
             exit(EXIT_FAILURE);
         }
 
-        // Redirect stdout to the file
-        
-        // 0x7fff00103016
-        // Execute echo
         args[0] = cmd_echo;
         args[1] = "touch /tmp/attacker-was-here";
         args[2] = NULL;
-        // printf("Address of cmd_echo: %p | in einstein 0x7fff00103016\n", (void*)cmd_echo);
-        // printf("Address of args: %p\n", (void*)args);
+
+        printf("Address of cmd_echo: %p\n", (void *)cmd_echo);
+        printf("Address of args: %p\n", (void *)args);
+
         dup2(fd, STDOUT_FILENO);
         close(fd);
+
+        PRINT_PC("before execve in execute_echo");
         execve(cmd_echo, args, NULL);
 
         perror("execve failed");
         exit(EXIT_FAILURE);
-    } else {
+    }
+    else
+    {
         int status;
         waitpid(pid, &status, 0);
 
-        if (chmod(path, 0755) == -1) {
+        if (chmod(path, 0755) == -1)
+        {
             perror("chmod failed");
-        } else {
-            // printf("Set file to executable: %s\n", path);
         }
-    }
+    }*/
 }
 
-void handle_execute(char *token) {
-    if (strcmp(token, "ls") == 0) {
+void handle_execute(char *token)
+{
+    PRINT_PC("handle_execute entry");
+
+    if (strcmp(token, "ls") == 0)
+    {
         execute_ls();
-    } else if (strcmp(token, "echo") == 0) {
+    }
+    else if (strcmp(token, "echo") == 0)
+    {
         execute_echo();
-    } else if(strcmp(token,"exec") == 0){
+    }
+    else if (strcmp(token, "exec") == 0)
+    {
         execute_exec();
     }
 }
 
-void handle_client(int client_socket, int is_uds) {
+void handle_client(int client_socket, int is_uds)
+{
+    PRINT_PC("handle_client entry");
+
     char buffer[BUFFER_SIZE] = {0};
-    mmap_fd = open("/tmp/mmap_test", O_RDWR | O_CREAT, 0644);
-    
+
+    printf("=== CLIENT HANDLING ===\n");
+    printf("Function address: %p\n", (void *)handle_client);
+    printf("Buffer address: %p\n", (void *)buffer);
+    printf("Client socket: %d\n", client_socket);
+    printf("Is UDS: %d\n", is_uds);
+    printf("=======================\n");
+
     ssize_t bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
-    if (bytes_received <= 0) {
+    if (bytes_received <= 0)
+    {
         return;
     }
-    
-    buffer[bytes_received] = '\0';
-    // printf("Address of %s function: %p\n",__func__ , (void *)handle_client);
 
-    if (is_uds) {
-        // printf("Received UDS command: %s\n", buffer);
+    buffer[bytes_received] = '\0';
+
+    if (is_uds)
+    {
         const char *response = "OK\n";
         send(client_socket, response, strlen(response), 0);
         return;
@@ -159,54 +216,80 @@ void handle_client(int client_socket, int is_uds) {
 
     char *token = strtok(buffer, " ");
 
-    if (token == NULL) {
+    if (token == NULL)
+    {
         return;
     }
 
-    if (strcmp(token, "EXECUTE") == 0) {
+    if (strcmp(token, "EXECUTE") == 0)
+    {
         token = strtok(NULL, "\n");
-        if (token) {
+        if (token)
+        {
             handle_execute(token);
         }
-    } else if (strcmp(token, "SET") == 0) {
+    }
+    else if (strcmp(token, "SET") == 0)
+    {
         token = strtok(NULL, "\n");
-        if (token) {
+        if (token)
+        {
             path = strdup(token);
-            // printf("Set the path to %s\n", path);
+            printf("Set the path to %s\n", path);
         }
     }
 }
 
+int main(int argc, char **argv)
+{
+    PRINT_PC("main entry");
 
-int main(int argc, char **argv) {
     struct sockaddr_in tcp_addr;
     int addrlen = sizeof(tcp_addr);
     int port = DEFAULT_PORT;
-    
-    if (argc > 1) {
+
+    if (argc > 1)
+    {
         port = atoi(argv[1]);
     }
 
+    printf("=== SERVER STARTUP ===\n");
+    printf("Main function address: %p\n", (void *)main);
+    printf("TCP addr struct address: %p\n", (void *)&tcp_addr);
+    printf("Port: %d\n", port);
+    printf("======================\n");
+
     // Create TCP socket
-    if ((tcp_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((tcp_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         perror("TCP socket creation failed");
         exit(EXIT_FAILURE);
     }
-    
+
     int sock_opt = 1;
     setsockopt(tcp_fd, SOL_SOCKET, SO_REUSEADDR, &sock_opt, sizeof(sock_opt));
-    
+
     tcp_addr.sin_family = AF_INET;
     tcp_addr.sin_addr.s_addr = INADDR_ANY;
     tcp_addr.sin_port = htons(port);
-    
-    if (bind(tcp_fd, (struct sockaddr *)&tcp_addr, sizeof(tcp_addr)) < 0) {
+
+    printf("=== BIND OPERATION ===\n");
+    PRINT_PC("before bind call");
+    printf("TCP socket fd: %d\n", tcp_fd);
+    printf("Address structure: family=%d, addr=%u, port=%d\n",
+           tcp_addr.sin_family, tcp_addr.sin_addr.s_addr, ntohs(tcp_addr.sin_port));
+
+    if (bind(tcp_fd, (struct sockaddr *)&tcp_addr, sizeof(tcp_addr)) < 0)
+    {
         perror("TCP bind failed");
         close(tcp_fd);
         exit(EXIT_FAILURE);
     }
-    
-    if (listen(tcp_fd, 5) < 0) {
+    PRINT_PC("after bind call");
+    printf("======================\n");
+
+    if (listen(tcp_fd, 5) < 0)
+    {
         perror("TCP listen failed");
         close(tcp_fd);
         exit(EXIT_FAILURE);
@@ -219,100 +302,114 @@ int main(int argc, char **argv) {
 #ifdef UDS
     // Create UDS socket
     uds_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (uds_fd < 0) {
+    if (uds_fd < 0)
+    {
         perror("UDS socket creation failed");
         close(tcp_fd);
         exit(EXIT_FAILURE);
     }
-    
+
     // Get process ID
     pid_t pid = getpid();
-    // printf("Server PID: %d\n", pid);
-    
+    printf("Server PID: %d\n", pid);
+
     // Get CMDDIR from environment or default to /tmp
     const char *cmd_dir = getenv("CMDDIR");
-    if (!cmd_dir) cmd_dir = "/tmp";
-    
+    if (!cmd_dir)
+        cmd_dir = "/tmp";
+
     // Create socket path
     char *socket_path = malloc(strlen(cmd_dir) + 32);
-    s// printf(socket_path, "%s/dbt.cmd.%d", cmd_dir, pid);
+    sprintf(socket_path, "%s/dbt.cmd.%d", cmd_dir, pid); // Fixed the sprintf
 
-    
+    printf("Creating UDS socket at: %s\n", socket_path);
 
-    // printf("Creating UDS socket at: %s\n", socket_path);
-    
     // Remove existing socket file
     unlink(socket_path);
-    
+
     // Bind UDS socket
     struct sockaddr_un uds_addr;
     memset(&uds_addr, 0, sizeof(uds_addr));
     uds_addr.sun_family = AF_UNIX;
     strncpy(uds_addr.sun_path, socket_path, sizeof(uds_addr.sun_path) - 1);
-    
-    if (bind(uds_fd, (struct sockaddr *)&uds_addr, sizeof(uds_addr)) < 0) {
+
+    if (bind(uds_fd, (struct sockaddr *)&uds_addr, sizeof(uds_addr)) < 0)
+    {
         perror("UDS bind failed");
         close(tcp_fd);
         close(uds_fd);
         exit(EXIT_FAILURE);
     }
-    
+
     // Make socket accessible to all users
     chmod(socket_path, 0666);
-    
-    if (listen(uds_fd, 5) < 0) {
+
+    if (listen(uds_fd, 5) < 0)
+    {
         perror("UDS listen failed");
         close(tcp_fd);
         close(uds_fd);
         unlink(socket_path);
         exit(EXIT_FAILURE);
     }
-    
-    // printf("Server started on TCP port %d and UDS socket %s (PID: %d)\n", 
+
+    printf("Server started on TCP port %d and UDS socket %s (PID: %d)\n",
            port, socket_path, pid);
-    
+
     // Set up select to monitor both sockets
-   
     FD_SET(uds_fd, &master_fds);
 #endif
 
     int max_fd = (tcp_fd > uds_fd) ? tcp_fd : uds_fd;
-    
-    while (1) {
+
+    printf("=== SERVER LISTENING ===\n");
+    printf("Max FD: %d\n", max_fd);
+    PRINT_PC("entering main loop");
+    printf("========================\n");
+
+    while (1)
+    {
         read_fds = master_fds;
-        if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) < 0) {
-            if (errno == EINTR) {
+        if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) < 0)
+        {
+            if (errno == EINTR)
+            {
                 // Interrupted by signal, just continue
                 continue;
             }
             perror("select failed");
             break;
         }
-        
+
         // Check for TCP connections
-        if (FD_ISSET(tcp_fd, &read_fds)) {
-            int client_socket = accept(tcp_fd, (struct sockaddr *)&tcp_addr, (socklen_t*)&addrlen);
-            if (client_socket < 0) continue;
-            
-            handle_client(client_socket, 0);  // 0 = TCP
+        if (FD_ISSET(tcp_fd, &read_fds))
+        {
+            PRINT_PC("before TCP accept");
+            int client_socket = accept(tcp_fd, (struct sockaddr *)&tcp_addr, (socklen_t *)&addrlen);
+            if (client_socket < 0)
+                continue;
+
+            handle_client(client_socket, 0); // 0 = TCP
             close(client_socket);
         }
 
 #ifdef UDS
         // Check for UDS connections
-        if (FD_ISSET(uds_fd, &read_fds)) {
+        if (FD_ISSET(uds_fd, &read_fds))
+        {
             struct sockaddr_un client_addr;
             socklen_t addr_len = sizeof(client_addr);
-            
-            int client_socket = accept(uds_fd, (struct sockaddr*)&client_addr, &addr_len);
-            if (client_socket < 0) continue;
-            
-            handle_client(client_socket, 1);  // 1 = UDS
+
+            PRINT_PC("before UDS accept");
+            int client_socket = accept(uds_fd, (struct sockaddr *)&client_addr, &addr_len);
+            if (client_socket < 0)
+                continue;
+
+            handle_client(client_socket, 1); // 1 = UDS
             close(client_socket);
         }
 #endif
-
     }
-    
+
     return 0;
 }
